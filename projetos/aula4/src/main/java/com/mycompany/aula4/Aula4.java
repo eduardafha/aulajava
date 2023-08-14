95e8974e52685e88568616563c88c8d51a0a119f
eduardafha <141193681+eduardafha@users.noreply.github.com>
2023-08-14 20:30:11 -0300

    Add files via upload

---
+++ projetos/aula4/src/main/java/com/mycompany/aula4/Aula4.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.aula4;
+
+/**
+ *
+ * @author 16856313632
+ */
+public class Aula4 {
+    
+  
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a = 10;
+  String b = "10";
+  int c = 2;
+  String d = "10";
+  
+  int resultadoA = a + c;
+  int resultadoB = a-c;
+  int resultadoC = a * c;
+  int resultadoD = a/ c;
+  int resultadoE = a % c;
+  int resultadoF = (int)a /c;
+          System.out.println(resultadoA);
+          System.out.println(resultadoB);
+          System.out.println(resultadoC);
+          System.out.println(resultadoD);
+          System.out.println(resultadoE);
+          System.out.println(resultadoF);
+          
+    }
+}
